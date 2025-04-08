bc3a297545f58f831d7f77734fc3cf3b2bdd9578
lina <nahyeon2252@gmail.com>
2025-04-08 18:00:07 +0900

    feat : 새로운 기능 추가

--- src/main/java/com/example/allin/controller/UserController.java
+++ src/main/java/com/example/allin/controller/UserController.java
@@ -1,15 +1,19 @@
 package com.example.allin.controller;
 
+import com.example.allin.dto.UserRequestDto;
+import com.example.allin.dto.UserResponseDto;
+import com.example.allin.service.UserService;
+import jakarta.validation.Valid;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
 
+    private final UserService userService;
 
 }
