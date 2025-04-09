9dc1d81d5542c116aee4f2dfa6de3896fb8b73ba
lina <nahyeon2252@gmail.com>
2025-04-09 21:03:38 +0900

    feat : 예외 처리 기능 추가

--- src/main/java/com/example/allin/exception/UserIdMismatchException.java
+++ src/main/java/com/example/allin/exception/UserIdMismatchException.java
@@ -3,7 +3,7 @@ package com.example.allin.exception;
 import lombok.Getter;
 
 @Getter
-public class UserIdMismatchException extends CustomException{
+public class UserIdMismatchException extends CustomException {
 
 
     public UserIdMismatchException(ErrorCode errorCode) {
