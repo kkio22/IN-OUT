5308c68f0afc4cc6eb3f5156ffb8463f84c1e2ec
Ciel415 <yun100778@gmail.com>
2025-04-10 15:20:31 +0900

    Update UserRepository.java

--- src/main/java/com/example/allin/repository/UserRepository.java
+++ src/main/java/com/example/allin/repository/UserRepository.java
@@ -3,7 +3,8 @@ package com.example.allin.repository;
 import com.example.allin.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
+import java.util.Optional; // 댓글쪽에서 필요해서 추가함
 
 public interface UserRepository extends JpaRepository<User, Long> {
+      Optional<User> findByEmail(String email); // 댓글쪽에서 필요해서 추가함
 }
