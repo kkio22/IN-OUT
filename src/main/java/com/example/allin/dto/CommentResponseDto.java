45a96577a2a96ec0be18ff3c6c0eab0096e067e8
Ciel415 <yun100778@gmail.com>
2025-04-11 20:14:16 +0900

    Update CommentResponseDto.java

--- src/main/java/com/example/allin/dto/CommentResponseDto.java
+++ src/main/java/com/example/allin/dto/CommentResponseDto.java
@@ -1,3 +1,23 @@
 package com.example.allin.dto;
 
-public record CommentResponseDto(String userName, String commentContent, int likeCount) {}
+import com.example.allin.entity.Comment;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class CommentResponseDto {
+    private Long id;
+    private String content;
+    private String username;
+    private LocalDateTime createdAt;
+    private Long likeCount;
+
+    public CommentResponseDto(Comment comment, Long likeCount) {
+        this.id = comment.getId();
+        this.content = comment.getContent();
+        this.username = comment.getUser().getUsername();
+        this.createdAt = comment.getCreatedAt();
+        this.likeCount = likeCount;
+    }
+}
