964e8523340970908fcd1cacafd3f7922d68f5b4
Ciel415 <yun100778@gmail.com>
2025-04-11 20:13:57 +0900

    Update CommentRequestDto.java

--- src/main/java/com/example/allin/dto/CommentRequestDto.java
+++ src/main/java/com/example/allin/dto/CommentRequestDto.java
@@ -1,15 +1,8 @@
 package com.example.allin.dto;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor
-public class CommentRequestDto{
-    private String commentContent;
-    private Long postId;
+public class CommentRequestDto {
+    private String content;
 }
-
